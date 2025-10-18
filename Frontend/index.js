e496b6b3c3054ab39e5ed2d489b46d649319e17a
Roland-Sankara <roland1sankara@gmail.com>
2025-10-18 21:17:04 +0300

    add cors package

---
+++ Frontend/index.js
@@ -0,0 +1,6 @@
+let apiUrl = "https://api-deployment-c7.onrender.com/data"
+fetch(apiUrl)
+.then((response)=>{
+    console.log(response)
+})
+
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const dataStore = require('./dataStore')
+const cors = require('cors')
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 // Fetch the data
 server.get('/data', (req, res)=>{
