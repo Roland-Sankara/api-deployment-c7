4e7bf41f60fbac03ba9fa8037a49146062e65ee3
Roland-Sankara <roland1sankara@gmail.com>
2025-10-18 21:32:32 +0300

    add frontend

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,6 +1,28 @@
 let apiUrl = "https://api-deployment-c7.onrender.com/data"
+
+// Add the tasks to the web site
+let tasksContainer = document.getElementById('tasks-box')
+
+function createListItem(item){
+    // new li element
+    let newLi = document.createElement('li')
+    newLi.innerText = item
+    return newLi
+}
+
 fetch(apiUrl)
 .then((response)=>{
-    console.log(response)
+    return response.json()
 })
+.then((data)=>{
+    console.log(data)
+    let tasks = data
+    let newTasks = tasks.map((task)=>{
+        return createListItem(task.title)
+    })
+
+    tasksContainer.append(...newTasks)
+})
+
+
 
